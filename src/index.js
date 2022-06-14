4e875ed38de870679a919870a9f80e6b8f44d688
honghanh999 <honghanh113999@gmail.com>
2022-06-14 10:09:24 +0700

    first commit

---
+++ src/index.js
@@ -0,0 +1,34 @@
+const express = require('express')
+const morgan = require('morgan')
+const {engine} = require('express-handlebars')
+const app = express()
+const port = 3000
+const path = require('path')
+
+app.use(express.static(path.join(__dirname, 'public')))
+// HTTP logger
+app.use(morgan('combined'))
+
+// Template engine
+app.engine('hbs', engine({
+  extname: '.hbs'
+}))
+app.set('view engine', 'hbs')
+app.set('views', path.join(__dirname, 'resources/views'))
+
+// app.get('/home', (req, res) => {
+//   res.send(`
+//   <h1 style="color:blue">Hello nha</h1>`)
+// })
+
+app.get('/', (req, res) => {
+  res.render('home')
+})
+
+app.get('/news', (req, res) => {
+  res.render('news')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+}) 
